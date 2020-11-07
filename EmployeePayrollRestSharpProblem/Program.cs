d06933503e85023ada8a25cb8640b4694bf89146
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-11-07 11:58:52 +0530

    [mrunal] Add. Added welcome message to main method

---
+++ EmployeePayrollRestSharpProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollRestSharpProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll RestSharp Problem!");
+        }
+    }
+}
