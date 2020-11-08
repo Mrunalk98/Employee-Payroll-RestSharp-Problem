0942e982004a207864b78499862da0b23d7907a1
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-11-08 23:39:17 +0530

    [mrunal Add. Added method to update salary in employee payroll

--- RestSharpTest/RestSharpTestCases.cs
+++ RestSharpTest/RestSharpTestCases.cs
@@ -53,8 +53,8 @@ namespace RestSharpTest
             // arrange
             RestRequest request = new RestRequest("/employees", Method.POST);
             JObject objectBody = new JObject();
-            objectBody.Add("name", "John");
-            objectBody.Add("salary", "18000");
+            objectBody.Add("name", "Johnny");
+            objectBody.Add("salary", "12000");
 
             request.AddParameter("application/json", objectBody, ParameterType.RequestBody);
 
@@ -64,8 +64,29 @@ namespace RestSharpTest
             // assert
             Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
             Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
-            Assert.AreEqual("John", dataResponse.name);
-            Assert.AreEqual("18000", dataResponse.salary);
+            Assert.AreEqual("Johnny", dataResponse.name);
+            Assert.AreEqual("12000", dataResponse.salary);
+        }
+
+        [TestMethod]
+        public void GivenEmployee_OnPut_ShouldReturnUpdatedEmployee()
+        {
+            // arrange
+            RestRequest request = new RestRequest("/employees/7", Method.PUT);
+            JObject objectBody = new JObject();
+            objectBody.Add("name", "Lavanya");
+            objectBody.Add("salary", "13000");
+
+            request.AddParameter("application/json", objectBody, ParameterType.RequestBody);
+
+            // act
+            IRestResponse response = client.Execute(request);
+
+            // assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Lavanya", dataResponse.name);
+            Assert.AreEqual("13000", dataResponse.salary);
         }
     }
 }
